1e511e2c0c36b9a6b7749b143427c26cc733e187
Shaju <shajupainkulam@gmail.com>
2021-09-28 16:48:54 +0530

    Color chenged

--- Js/index.js
+++ Js/index.js
@@ -1,12 +1,12 @@
 
 let CLOSE_NAV_DRAWER = () => {
 
-    document.getElementById("navDrawer").className = "nav-drawer-hidden";
+  document.getElementById("navDrawer").className = "nav-drawer-hidden";
 }
 
 let OPEN_NAV_DRAWER = () => {
 
-    document.getElementById("navDrawer").className = "nav-drawer";
+  document.getElementById("navDrawer").className = "nav-drawer";
 }
 
 
@@ -14,31 +14,52 @@ var myIndex = 0;
 window.onload = function () {
 
 
-    startSliding();
+  startSliding();
+
+
+
 }
 
+$(window).on("scroll", function () {
+
+  if ($(window).width() > 767) {
+    if ($(window).scrollTop() > $(".image-slider").height() - $(".header").height()) {
+      jQuery('.header').css('background-color', '#000000');
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      jQuery('.header').css('background-color', '#000000a2');
+    }
+
+  } else {
+
+  }
+
+
+
+
+});
 
 
 
 let startSliding = () => {
 
-    let images_count = $("#slderImages > img").length
-    var x = document.getElementById("slderImages").children;
+  let images_count = $("#slderImages > img").length
+  var x = document.getElementById("slderImages").children;
 
 
-    for (i = 0; i < images_count; i++) {
-        //console.log("hiding",i);
-        x[i].className = "slide-image-hide";
-    }
+  for (i = 0; i < images_count; i++) {
+    //console.log("hiding",i);
+    x[i].className = "slide-image-hide";
+  }
 
-    myIndex++;
+  myIndex++;
 
-    if (myIndex > images_count) { myIndex = 1 }
+  if (myIndex > images_count) { myIndex = 1 }
 
-    var y = document.getElementById("slderImages").children;
-    //console.log(myIndex,y[myIndex-1]);
-    y[myIndex - 1].className = "slide-image";
-    setTimeout(startSliding, 5000); // Change image every 4 seconds
+  var y = document.getElementById("slderImages").children;
+  //console.log(myIndex,y[myIndex-1]);
+  y[myIndex - 1].className = "slide-image";
+  setTimeout(startSliding, 5000); // Change image every 4 seconds
 
 
 
